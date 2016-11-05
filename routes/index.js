f1857488663976337fb76092c595646cd943d285
Ahyana Rizky Pratama <ahyana.rizky@gmail.com>
2016-11-05 10:59:36 +0700

    add register and login view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {title: 'Auth System'});
+});
+
+router.get('/register', function(req, res, next) {
+    res.render('register', {title: 'Auth System'});
+});
+
+router.get('/login', function(req, res, next) {
+    res.render('login', {title: 'Auth System'});
 });
 
 module.exports = router;
