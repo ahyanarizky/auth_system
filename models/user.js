0130847f3a8f5c11d1bc2348061f24c8a3b8e149
Ahyana Rizky Pratama <ahyana.rizky@gmail.com>
2016-11-05 11:48:48 +0700

    login scheme tested

--- models/user.js
+++ models/user.js
@@ -8,6 +8,11 @@ const User = new Schema({
         type: String,
         required: true
     },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
     email: {
         type: String,
         required: true,
@@ -19,28 +24,8 @@ const User = new Schema({
         },
         unique: true
     },
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
     password: {
         type: String,
-        required: true,
-        validate: {
-            validator: function(email) {
-                return /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email)
-            },
-            message: `format email salah`
-        },
-        unique: true
-    },
-    role: {
-        type: String,
-        required: true
-    },
-    birthdate: {
-        type: Date,
         required: true
     }
 })
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,10 +11,44 @@ router.get('/register', function(req, res, next) {
     res.render('register', {title: 'Auth System'});
 });
 
-router.post('/register', function(req, res, next) {});
+router.post('/register', function(req, res, next) {
+    if (req.body.password == req.body.confirmpassword) {
+        User.register({
+            fullname: req.body.fullname,
+            username: req.body.username,
+            email: req.body.email
+        }, req.body.password, function(err, result) {
+            if (err) {
+                console.log(err);
+                res.render('register', {alert: 'Registration Failed. Please Try again'})
+            } else {
+                passport.authenticate('local')(req, res, function() {
+                    req.session.save(function(err, next) {
+                        if (err)
+                            return next(err)
+                        res.redirect('/dashboard')
+                    })
+                })
+            }
+        })
+
+    } else {
+        res.render('register', {alert: 'Confirm Password not matched'})
+    }
+});
 
 router.get('/login', function(req, res, next) {
     res.render('login', {title: 'Auth System'});
+});
+
+router.post('/login', function(req, res, next) {
+    req.session.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/login')
+        }
+    })
 });
 
 module.exports = router;
